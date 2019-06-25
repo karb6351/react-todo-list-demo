740c173db63499dfc4f5a34f741257019b750e6b
B Kar <karb6351@gmail.com>
2019-06-26 04:54:38 +0800

    change list and listitem to stateless component

--- src/List.js
+++ src/List.js
@@ -1,36 +1,34 @@
-import React, {Component} from 'react';
+import React from 'react';
 import ListItem from "./ListItem";
 
-class List extends Component {
-  render() {
-    return (
-      <div className="card">
-        <div className="card-header">
-          Todo List
-        </div>
-        <table className="table">
-          <thead>
-          <tr>
-            <th>Label</th>
-            <th>Status</th>
-            <th>Actions</th>
-          </tr>
-          </thead>
-          <tbody>
-          {this.props.list.map((item, index) => (
-            <ListItem
-              index={index} key={index} item={item}
-              handleChangeLabel={(e, index) => this.props.handleChangeLabel(e, index)}
-              handleIsEditing={(index) => this.props.handleIsEditing(index)}
-              handleDelete={(index) => this.props.handleDelete(index)}
-              handleStatus={(index) => this.props.handleStatus(index)}
-            />)
-          )}
-          </tbody>
-        </table>
+function List({ handleChangeLabel, handleIsEditing, handleDelete, handleStatus, list }){
+  return (
+    <div className="card">
+      <div className="card-header">
+        Todo List
       </div>
-    );
-  }
+      <table className="table">
+        <thead>
+        <tr>
+          <th>Label</th>
+          <th>Status</th>
+          <th>Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+        {list.map((item, index) => (
+          <ListItem
+            index={index} key={index} item={item}
+            handleChangeLabel={(e, index) => handleChangeLabel(e, index)}
+            handleIsEditing={(index) => handleIsEditing(index)}
+            handleDelete={(index) => handleDelete(index)}
+            handleStatus={(index) => handleStatus(index)}
+          />)
+        )}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default List;
--- src/ListItem.js
+++ src/ListItem.js
@@ -1,25 +1,22 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class ListItem extends Component {
-  render() {
-    const { label, done, isEditing } = this.props.item;
-    const { handleChangeLabel, handleIsEditing, handleDelete, handleStatus, index } = this.props;
-    return (
-      <tr>
-        <td style={{verticalAlign: 'middle'}}>
-          <div className="form-group">
-            <input type="text" readOnly={!isEditing} className={`form-control${ !isEditing ? '-plaintext' : '' }`} value={label} onChange={(e) => handleChangeLabel(e, index)} />
-          </div>
-        </td>
-        <td style={{verticalAlign: 'middle'}}><span className={`badge badge-pill badge-${done ? 'success' : 'secondary'}`}>{done ? 'Finish' : 'Not Finish'}</span></td>
-        <td style={{verticalAlign: 'middle'}}>
-          <button onClick={() => handleIsEditing(index)} style={{marginRight: 5}} className="btn btn-outline-info">Edit</button>
-          <button onClick={() => handleStatus(index)} style={{marginRight: 5}} className={`btn btn-outline-${done ? 'primary' : 'secondary'}`}>{done ? 'Mark as unfinished' : 'Mark as done' }</button>
-          <button onClick={() => handleDelete(index)} className="btn btn-outline-danger">delete</button>
-        </td>
-      </tr>
-    );
-  }
+function ListItem({ item: { label, done, isEditing }, handleChangeLabel, handleIsEditing, handleDelete, handleStatus, index }){
+  return (
+    <tr>
+      <td style={{verticalAlign: 'middle'}}>
+        <div className="form-group">
+          <input type="text" readOnly={!isEditing} className={`form-control${ !isEditing ? '-plaintext' : '' }`} value={label} onChange={(e) => handleChangeLabel(e, index)} />
+        </div>
+      </td>
+      <td style={{verticalAlign: 'middle'}}><span className={`badge badge-pill badge-${done ? 'success' : 'secondary'}`}>{done ? 'Finish' : 'Not Finish'}</span></td>
+      <td style={{verticalAlign: 'middle'}}>
+        <button onClick={() => handleIsEditing(index)} style={{marginRight: 5}} className="btn btn-outline-info">Edit</button>
+        <button onClick={() => handleStatus(index)} style={{marginRight: 5}} className={`btn btn-outline-${done ? 'primary' : 'secondary'}`}>{done ? 'Mark as unfinished' : 'Mark as done' }</button>
+        <button onClick={() => handleDelete(index)} className="btn btn-outline-danger">delete</button>
+      </td>
+    </tr>
+  );
 }
+
 
 export default ListItem;
